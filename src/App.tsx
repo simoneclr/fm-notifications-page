6f653f8136b94d36b8bb9b15a088bdd267ecd1c6
simoneclr <simone.clclr@gmail.com>
2022-09-19 16:35:35 +0200

    Extend tailwind.config.js with provided style guide

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="App">
+    <div className="App font-jakarta text-darkGrayishBlue">
       <h1>Hello World!</h1>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        customRed: "hsl(1, 90%, 64%)",
+        customBlue: "hsl(219, 85%, 26%)",
+        veryLightGrayishBlue: "hsl(210, 60%, 98%)",
+        lightGrayishBlue: "hsl(211, 68%, 94%)",
+        lightGrayishBlueTwo: "hsl(205, 33%, 90%)",
+        grayishBlue: "hsl(219, 14%, 63%)",
+        darkGrayishBlue: "hsl(219, 12%, 42%)",
+        veryDarkBlue: "hsl(224, 21%, 14%)"
+      },
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
