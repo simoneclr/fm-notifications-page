267e2fa7553e939bd786a49dfe8f92109106cd34
simoneclr <simone.clclr@gmail.com>
2022-09-20 15:28:27 +0200

    Add some layout to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
-    <div className="App font-jakarta text-darkGrayishBlue">
-      <h1>Hello World!</h1>
+    <div className="App min-h-screen font-jakarta text-darkGrayishBlue bg-veryLightGrayishBlue lg:py-12">
+      <div className="max-w-[1440px] mx-auto">
+        <main className="bg-white mx-auto lg:p-8 lg:rounded-xl lg:shadow-xl">
+          <h1>Hello World!</h1>
+        </main>
+      </div>
     </div>
   );
 }
