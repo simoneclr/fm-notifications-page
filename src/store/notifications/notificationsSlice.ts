4edf8b7ed501a584bcdf109ef61622338d1db8a4
simoneclr <simone.clclr@gmail.com>
2022-09-20 15:36:30 +0200

    Add sortComparer to notificationsSlice

--- src/store/notifications/notificationsSlice.ts
+++ src/store/notifications/notificationsSlice.ts
@@ -2,7 +2,9 @@ import { createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 
 import { MyNotification } from "../../model/notifications";
 
-const notificationsAdapter = createEntityAdapter<MyNotification>({})
+const notificationsAdapter = createEntityAdapter<MyNotification>({
+	sortComparer: (a, b) => b.date.localeCompare(a.date)
+})
 
 const notificationsSlice = createSlice({
 	name: "notifications",
